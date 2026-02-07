1533e7eab41d30ab51552c9d7e9dff7a31e7c468
Necobgs <emanuelborgesdev@gmail.com>
2026-02-07 12:30:24 -0300

    fix: npm run dist

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-const router = createBrowserRouter([
+
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
